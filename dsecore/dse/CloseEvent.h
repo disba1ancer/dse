15634c2fab6f778d8d2bf91369fe28dbaa09593a
disba1ancer <antonbobachev@gmail.com>
2019-03-26 23:55:22 +0300

    Added license information

--- dsecore/dse/CloseEvent.h
+++ dsecore/dse/CloseEvent.h
@@ -1,8 +1,25 @@
+/*******************************************************************************
+ * DSE - disba1ancer's (graphic) engine.
+ *
+ * Copyright (c) 2019 ${user}.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *******************************************************************************/
 /*
  * CloseEvent.h
  *
  *  Created on: 3 окт. 2018 г.
- *      Author: Anton
+ *      Author: disba1ancer
  */
 
 #ifndef CLOSEEVENT_H_
