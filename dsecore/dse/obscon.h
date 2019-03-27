ceef7a95b69d51a98a943208b6dc28c125445b24
disba1ancer <antonbobachev@gmail.com>
2019-03-27 19:51:22 +0300

    Fixed copyright notices

--- dsecore/dse/obscon.h
+++ dsecore/dse/obscon.h
@@ -1,7 +1,7 @@
 /*******************************************************************************
  * DSE - disba1ancer's (graphic) engine.
  *
- * Copyright (c) 2019 ${user}.
+ * Copyright (c) 2019 disba1ancer.
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
